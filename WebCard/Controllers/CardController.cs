1dbf407264540881b458e72d0faddcebed4f0f56
ProdamGaraj <prodgaraj@yandex.ru>
2020-06-14 22:15:36 +0300

    Add controller and views.

---
+++ WebCard/Controllers/CardController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Entities;
+using Infrastructure;
+namespace WebCard.Controllers
+{
+    [Route("html/[controller]")]
+    public class CardController : Controller
+    {
+        private ICardRepository _cardRepository { get; set; }
+
+        public CardController(ICardRepository cardRepository)
+        {
+            _cardRepository = cardRepository;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            return View(_cardRepository.Get(id));
+        }
+
+        [HttpGet("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public ActionResult Create([FromBody] Card card)
+        {
+            try
+            {
+                _cardRepository.Add(card);
+                return Ok();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet("update/{id}")]
+        public ActionResult Edit(int id)
+        {
+            return View(_cardRepository.Get(id));
+        }
+        [HttpPut("{id}")]
+        public ActionResult Edit(int id, [FromBody] Card card)
+        {
+            try
+            {
+                _cardRepository.Update(card);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        [HttpGet("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            return View(_cardRepository.Get(id));
+        }
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromBody] Card card)
+        {
+            try
+            {
+                _cardRepository.Delete(card);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
